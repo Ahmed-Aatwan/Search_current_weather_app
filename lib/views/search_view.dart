168426838314ede2ab6e2b9adff570fa60d7a9df
Aatwan <ahmedatwaan2@gmail.com>
2025-04-21 10:48:22 +0200

    unused service removed

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_search_app/cubits/get_weather_cubit/get_weather_cubit.dart';
-import 'package:weather_search_app/models/weather_model.dart';
 import 'package:weather_search_app/services/weather_service.dart'; // Import the WeatherService
 
 class SearchView extends StatelessWidget {
